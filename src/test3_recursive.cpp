5fb86969c0e32a0c34fab0aaa2cef18dead8825f
Alexander Abrosov <aabrosov@gmail.com>
2022-09-21 16:21:10 +0300

    recursive version fully reworked now it works very fast

--- src/test3_recursive.cpp
+++ src/test3_recursive.cpp
@@ -3,188 +3,325 @@
 #include <array>
 #include <set>
 
-#define SIZE_X 3
-#define SIZE_Y 3
-
-/*
-TODO:
-convert coordinates from {x, y} to uint_32 x + (y << 16)
-convert score from {square, counter} to uint_32 square + (counter << 16)
-done
-
-*/
-
 using namespace std;
 
-typedef vector <uint32_t> result_type;
-
-result_type add_results(result_type results1, result_type results2) {
-    // will add content
-    result_type results;
-    for (uint32_t & result1 : results1) {
-        uint32_t s1 = result1 & 0x0000FFFF;
-        uint32_t c1 = result1 >> 16;
-        for (uint32_t & result2 : results2) {
-            uint32_t s2 = result1 & 0x0000FFFF;
-            uint32_t c2 = result1 >> 16;
-            results.push_back(s1 + s2 + ((c1 + c2) << 16));
-        }
-    }
-    return results;
+uint32_t pack_4(uint8_t n1, uint8_t n2, uint8_t n3, uint8_t n4) {
+    return n4 | (n3 << 8) | (n2 << 16) | (n1 << 24);
 }
 
-result_type pack_results(result_type results, uint32_t & map_size, uint32_t & rects_size) {
-    // collect uniq squares
-    set <uint32_t> uniq_squares;
-    for (auto & result : results) {
-        uniq_squares.insert(result & 0x0000FFFF);
+array <uint8_t, 4> unpack_4(uint32_t n1n2n3n4) {
+    array <uint8_t, 4> data;
+    data[0] = (n1n2n3n4 >> 24);
+    data[1] = (n1n2n3n4 >> 16) & 0xFF;
+    data[2] = (n1n2n3n4 >>  8) & 0xFF;
+    data[3] = n1n2n3n4 & 0xFF;
+    return data;
+}
+
+void print_rect(uint32_t rect) {
+    array <uint8_t, 4> coords = unpack_4(rect);
+    cout << "(" << int(coords[0]) << "'" << int(coords[1]);
+    cout << "'" << int(coords[2]) << "'" << int(coords[3]) << ")" << endl;
+}
+
+array <uint16_t, 2> unpack_2(uint32_t num1num2) {
+    array <uint16_t, 2> data;
+    data[0] = (num1num2 >> 16);
+    data[1] = num1num2 & 0xFFFF;
+    return data;
+}
+
+void check_solution(uint32_t data, set <uint32_t> & prev) {
+    // Unpack data
+    array <uint8_t, 4> coords = unpack_4(data);
+    uint8_t t = coords[0];
+    uint8_t n = coords[1];
+    uint8_t m = coords[2];
+    uint8_t p = coords[3];
+    for (auto it = prev.begin(); it != prev.end(); ++it) {
+        auto solution = * it;
+        array <uint16_t, 2> scores = unpack_2(solution);
+        uint16_t square = scores[1];
+        uint16_t counter = scores[0];
+        if (square > n * m || counter > p) {
+            prev.erase(it);
+            --it;
+        }
+    }
+}
+
+set <uint32_t> walkthrough(uint32_t data, set <uint32_t> prev, uint32_t rect, vector <uint32_t> & rects) {
+    check_solution(data, prev);
+    // Unpack info about current rectangle
+    // Coords
+    array <uint8_t, 4> coords = unpack_4(rect);
+    uint8_t x1 = coords[0];
+    uint8_t y1 = coords[1];
+    uint8_t x2 = coords[2];
+    uint8_t y2 = coords[3];
+    // Vertices
+    uint16_t ll = (x1 << 8) | y1;
+    uint16_t lr = (x2 << 8) | y1;
+    uint16_t ul = (x1 << 8) | y2;
+    uint16_t ur = (x2 << 8) | y2;
+    // Square
+    uint16_t square = (x2 - x1) * (y2 - y1);
+    // Zero-point solution = prev
+    uint16_t counter = 1;
+    uint32_t score = square | (counter << 16);
+    prev.insert(score);
+
+    // Search of children rectangles
+    set <uint32_t> candidate_solution;
+    // Coords
+    uint16_t candidate_ll, candidate_lr, candidate_ul, candidate_ur;
+    // Vectors for candidates processing
+    vector <uint32_t> candidates_ll, candidates_lr, candidates_ul, candidates_ur;
+    // Iterate through the candidates
+    for (auto & candidate : rects) {
+        // Will not process the same rects
+        if (candidate == rect) continue;
+        // Unpack info about candidate
+        // Coords
+        // uint32_t candidate = x1_y1_x2_y2
+        uint8_t candidate_x1 = (candidate >> 24);
+        uint8_t candidate_y1 = (candidate >> 16) & 0xFF;
+        uint8_t candidate_x2 = (candidate >>  8) & 0xFF;
+        uint8_t candidate_y2 = candidate & 0xFF;
+        // Vertices
+        uint16_t candidate_ll = (candidate_x1 << 8) | candidate_y1;
+        uint16_t candidate_lr = (candidate_x2 << 8) | candidate_y1;
+        uint16_t candidate_ul = (candidate_x1 << 8) | candidate_y2;
+        uint16_t candidate_ur = (candidate_x2 << 8) | candidate_y2;
+        // Square
+        int candidate_s = (candidate_x2 - candidate_x1) * (candidate_y2 - candidate_y1);
+        // Counter
+        uint16_t candidate_c = 1;
+        // Score
+        uint32_t candidate_sc = candidate_s | (candidate_c << 16);
+        // Zero-point solution
+        candidate_solution.insert(candidate_sc);
+        // Push candidates rects to vectors
+        if (candidate_ll == ll) candidates_ll.push_back(candidate);
+        if (candidate_ll == lr) candidates_lr.push_back(candidate);
+        if (candidate_ll == ul) candidates_ul.push_back(candidate);
+        if (candidate_ll == ur) candidates_ur.push_back(candidate);
     }
 
-    // merge good and remove bad results
-    uint32_t min_counter;
-    result_type new_results;
-    for (auto & result : results) {
-        auto square = result & 0x0000FFFF;
-        auto counter = result >> 16;
-        if (square <= map_size && square != 0 && counter <= rects_size && counter != 0) {
-            min_counter = rects_size;
-            for (auto & curr_square : uniq_squares) {
-                min_counter = min(min_counter, counter);
+    // Flags for kids presenñe
+    bool ll_f, lr_f, ul_f, ur_f;
+    ll_f = candidates_ll.size() == 0;
+    lr_f = candidates_lr.size() == 0;
+    ul_f = candidates_ul.size() == 0;
+    ur_f = candidates_ur.size() == 0;
+
+    // Exit without candidates
+    if (ll_f && lr_f && ul_f && ur_f) {
+        // Remove rect from list if no children
+        for (auto it = rects.begin(); it != rects.end(); ++it) {
+            if (* it == rect) {
+                rects.erase(it);
+                --it;
             }
-            new_results.push_back(square + (min_counter << 16));
         }
-    }
-    return new_results;
-}
-
-result_type walkthrough(uint32_t & n, uint32_t & m, uint32_t & i, vector <array <uint32_t, 4>> & xrects) {
-    // vertices of current rectangle
-    uint32_t ll_rect, lr_rect, ul_rect, ur_rect;
-//    for (uint32_t i = 0; i < xrects.size(); ++i) {
-        ll_rect = xrects[i][0];
-        lr_rect = xrects[i][1];
-        ul_rect = xrects[i][2];
-        ur_rect = xrects[i][3];
-//    }
-    // lists of child rectangles
-    vector <uint32_t> lr_index, ul_index, ur_index;
-    for (uint32_t i = 0; i < xrects.size(); ++i) {
-        if (lr_rect == ll_rect) lr_index.push_back(i);
-        if (ul_rect == ll_rect) ul_index.push_back(i);
-        if (ur_rect == ll_rect) ur_index.push_back(i);
+        return candidate_solution;
     }
 
-    // recursion magic
-    uint32_t map_size = n * m;
-    uint32_t rects_size = xrects.size();
-    uint32_t x1 = xrects[i][0] & 0x0000FFFF;
-    uint32_t y1 = xrects[i][0] >> 16;
-    uint32_t x2 = xrects[i][3] & 0x0000FFFF;
-    uint32_t y2 = xrects[i][3] >> 16;
-    uint32_t square = (x2 - x1) * (y2 - y1);
+    // Set for saving all solutions
+    set <uint32_t> scores;
 
-    result_type current_rect;
-    current_rect = {square + (1 << 16)};
+    // temporary sets for calculations
+    set <uint32_t> tmp_xx, tmp0, tmp_ll, tmp_lr, tmp_ul, tmp_ur;
 
-    if        (lr_index.size() == 0 && ul_index.size() == 0 && ur_index.size() == 0) {
-        return current_rect;
-    } else if (lr_index.size() != 0 && ul_index.size() == 0 && ur_index.size() == 0) {
-        result_type lr_tmp; for (auto & lr_i : lr_index) lr_tmp = walkthrough(n, m, lr_i, xrects);
-        return add_results(current_rect, lr_tmp);
-    } else if (lr_index.size() == 0 && ul_index.size() != 0 && ur_index.size() == 0) {
-        result_type ul_tmp; for (auto & ul_i : ul_index) ul_tmp = walkthrough(n, m, ul_i, xrects);
-        return add_results(current_rect, ul_tmp);
-    } else if (lr_index.size() == 0 && ul_index.size() == 0 && ur_index.size() != 0) {
-        result_type ur_tmp; for (auto & ur_i : ur_index) ur_tmp = walkthrough(n, m, ur_i, xrects);
-        return add_results(current_rect, ur_tmp);
-    } else if (lr_index.size() == 0 && ul_index.size() != 0 && ur_index.size() != 0) {
-        result_type ul_tmp; for (auto & ul_in : ul_index) ul_tmp = walkthrough(n, m, ul_in, xrects);
-        ul_tmp = add_results(current_rect, ul_tmp);
-        result_type ur_tmp; for (auto & ur_in : ur_index) ur_tmp = walkthrough(n, m, ur_in, xrects);
-        ur_tmp = add_results(current_rect, ur_tmp);
-        return pack_results(add_results(ul_tmp, ur_tmp), map_size, rects_size);
-    } else if (lr_index.size() != 0 && ul_index.size() == 0 && ur_index.size() != 0) {
-        result_type lr_tmp; for (auto & lr_in : lr_index) lr_tmp = walkthrough(n, m, lr_in, xrects);
-        lr_tmp = add_results(current_rect, lr_tmp);
-        result_type ur_tmp; for (auto & ur_in : ur_index) ur_tmp = walkthrough(n, m, ur_in, xrects);
-        ur_tmp = add_results(current_rect, ur_tmp);
-        return pack_results(add_results(lr_tmp, ur_tmp), map_size, rects_size);
-    } else if (lr_index.size() != 0 && ul_index.size() != 0 && ur_index.size() == 0) {
-        result_type lr_tmp; for (auto & lr_in : lr_index) lr_tmp = walkthrough(n, m, lr_in, xrects);
-        lr_tmp = add_results(current_rect, lr_tmp);
-        result_type ul_tmp; for (auto & ul_in : ul_index) ul_tmp = walkthrough(n, m, ul_in, xrects);
-        ul_tmp = add_results(current_rect, ul_tmp);
-        return pack_results(add_results(lr_tmp, ul_tmp), map_size, rects_size);
-    } else if (lr_index.size() != 0 && ul_index.size() != 0 && ur_index.size() != 0) {
-        result_type lr_tmp; for (auto & lr_ind : lr_index) lr_tmp = walkthrough(n, m, lr_ind, xrects);
-        result_type ul_tmp; for (auto & ul_ind : ul_index) ul_tmp = walkthrough(n, m, ul_ind, xrects);
-        result_type ur_tmp; for (auto & ur_ind : ur_index) ur_tmp = walkthrough(n, m, ur_ind, xrects);
-        // will merge it
-        result_type lr_ul_ur_res;
-        lr_ul_ur_res.push_back(square + (1 << 16));
-        for (auto & result1 : lr_tmp) {
-        lr_ul_ur_res.push_back((result1 & 0x0000FFFF + square) + (((result1 >> 16) + 1) << 16));
-        for (auto & result2 : ul_tmp) {
-        lr_ul_ur_res.push_back((result1 & 0x0000FFFF + result2 & 0x0000FFFF + square) + (((result1 >> 16) + (result2 >> 16) + 1) << 16));
-        for (auto & result3 : ur_tmp) {
-        lr_ul_ur_res.push_back((result2 & 0x0000FFFF + square) + (((result2 >> 16 + 1)) << 16));
-        lr_ul_ur_res.push_back((result3 & 0x0000FFFF + square) + (((result3 >> 16 + 1)) << 16));
-        lr_ul_ur_res.push_back((result2 & 0x0000FFFF + result3 & 0x0000FFFF + square) + (((result2 >> 16) + (result3 >> 16) + 1) << 16));
-        lr_ul_ur_res.push_back((result1 & 0x0000FFFF + result3 & 0x0000FFFF + square) + (((result1 >> 16) + (result3 >> 16) + 1) << 16));
-        lr_ul_ur_res.push_back((result1 & 0x0000FFFF + result2 & 0x0000FFFF + result3 & 0x0000FFFF + square) + (((result1 >> 16) + (result2 >> 16) + (result3 >> 16) + 1) << 16));
+    // If only one vertex lr with children
+    if (ll_f && !lr_f && ul_f && ur_f) {
+        tmp_xx.clear();
+        // Iterate through these children
+        for (auto & lr : candidates_lr) {
+            tmp_lr = walkthrough(data, prev, lr, rects);
+            for (auto & x1 : prev) {
+                for (auto & x2 : tmp_lr) {
+                    tmp_xx.insert(x1 + x2);
+                }
+            }
         }
-        }
-        }
-        return pack_results(lr_ul_ur_res, map_size, rects_size);
-    } else {
-        // shouldn't be here
-        return current_rect;
+        return tmp_xx;
     }
+
+    // If only one vertex ul with children
+    if (ll_f && lr_f && !ul_f && ur_f) {
+        tmp_xx.clear();
+        // Iterate through these children
+        for (auto & ul : candidates_ul) {
+            tmp_ul = walkthrough(data, prev, ul, rects);
+            for (auto & x1 : prev) {
+                for (auto & x2 : tmp_ul) {
+                    tmp_xx.insert(x1 + x2);
+                }
+            }
+        }
+        return tmp_xx;
+    }
+
+    // If only one vertex ur with children
+    if (ll_f && lr_f && ul_f && !ur_f) {
+        tmp_xx.clear();
+        // Iterate through these children
+        for (auto & ur : candidates_ur) {
+            tmp_ur = walkthrough(data, prev, ur, rects);
+            for (auto & x1 : prev) {
+                for (auto & x2 : tmp_ul) {
+                    tmp_xx.insert(x1 + x2);
+                }
+            }
+        }
+        return tmp_xx;
+    }
+
+    // If two vertices with children lr ul 
+    if (ll_f && !lr_f && !ul_f && ur_f) {
+        tmp_xx.clear();
+        for (auto & lr : candidates_lr) tmp_lr = walkthrough(data, prev, lr, rects);
+        for (auto & ul : candidates_ul) tmp_ul = walkthrough(data, prev, ul, rects);
+        for (auto & x1 : tmp_lr) {
+            for (auto & x2 : tmp_ul) {
+                tmp_xx.insert(score);
+                tmp_xx.insert(x1 + score);
+                tmp_xx.insert(x2 + score);
+                tmp_xx.insert(x1 + x2 + score);
+            }
+        }
+        return tmp_xx;
+    }
+
+    // If two vertices with children lr ur 
+    if (ll_f && !lr_f && ul_f && !ur_f) {
+        tmp_xx.clear();
+        for (auto & lr : candidates_lr) tmp_lr = walkthrough(data, prev, lr, rects);
+        for (auto & ur : candidates_ur) tmp_ur = walkthrough(data, prev, ur, rects);
+        for (auto & x1 : tmp_lr) {
+            for (auto & x2 : tmp_ur) {
+                tmp_xx.insert(score);
+                tmp_xx.insert(x1 + score);
+                tmp_xx.insert(x2 + score);
+                tmp_xx.insert(x1 + x2 + score);
+            }
+        }
+        return tmp_xx;
+    }
+
+    // If two vertices with children ul ur
+    if (ll_f && lr_f && !ul_f && !ur_f) {
+        tmp_xx.clear();
+        for (auto & ul : candidates_ul) tmp_ul = walkthrough(data, prev, ul, rects);
+        for (auto & ur : candidates_ur) tmp_ur = walkthrough(data, prev, ur, rects);
+        for (auto & x1 : tmp_ul) {
+            for (auto & x2 : tmp_ur) {
+                tmp_xx.insert(score);
+                tmp_xx.insert(x1 + score);
+                tmp_xx.insert(x2 + score);
+                tmp_xx.insert(x1 + x2 + score);
+            }
+        }
+        return tmp_xx;
+    }
+
+    // If three vertices with children
+    if (ll_f && !lr_f && !ul_f && !ur_f) {
+        tmp_xx.clear();
+        for (auto & lr : candidates_lr) tmp_lr = walkthrough(data, prev, lr, rects);
+        for (auto & ul : candidates_ul) tmp_ul = walkthrough(data, prev, ul, rects);
+        for (auto & ur : candidates_ur) tmp_ur = walkthrough(data, prev, ur, rects);
+        for (auto & x1 : tmp_lr) {
+            for (auto & x2 : tmp_ul) {
+                for (auto & x3 : tmp_ur) {
+                    tmp_xx.insert(score);
+                    tmp_xx.insert(x1 + score);
+                    tmp_xx.insert(x2 + score);
+                    tmp_xx.insert(x3 + score);
+                    tmp_xx.insert(x1 + x2 + score);
+                    tmp_xx.insert(x2 + x3 + score);
+                    tmp_xx.insert(x3 + x1 + score);
+                    tmp_xx.insert(x1 + x2 + x3 + score);
+                }
+            }
+        }
+        return tmp_xx;
+    }
+    // couldn't be here
+    return tmp_xx;
 } 
 
 int main() {
     // Working data
-    vector <array <uint32_t, 4>> rects;
-    array <uint32_t, 4> coords;
+//    uint8_t t = 1, n = 3, m = 3, p = 5;
+    uint8_t t = 1, n = 6, m = 6, p = n*m;
+    uint32_t data = pack_4(t, n, m, p);
 
-    // constants
-    uint32_t n = 3;
-    uint32_t m = 3;
-    uint32_t p = 5;
+    // Print working data
+    cout << "t=" << int(t) << ", n=" << int(n) << ", m=" << int(m) << ", p=" << int(p) << "." << endl;
 
-    //rects.clear();
-    coords = {0, 0, 2, 1}; rects.push_back(coords);
-    coords = {2, 0, 3, 2}; rects.push_back(coords);
-    coords = {0, 1, 1, 3}; rects.push_back(coords);
-    coords = {1, 2, 3, 3}; rects.push_back(coords);
-    coords = {1, 1, 2, 2}; rects.push_back(coords);
+    // Rects data
+    vector <uint32_t> rects;
 
-    vector <array <uint32_t, 4>> xrects;
-    array <uint32_t, 4> vertices;
-    for (uint32_t i = 0; i < rects.size(); ++i) {
-        auto rect = rects[i];
-        vertices = {rect[0] + (rect[1] << 16),
-                    rect[2] + (rect[1] << 16),
-                    rect[0] + (rect[3] << 16),
-                    rect[2] + (rect[3] << 16)};
-        xrects.push_back(vertices);
+    for(int i=0;i<n;i+=2)
+    for(int j=0;j<m;j+=2)
+    rects.push_back(pack_4(i,j,i+2,j+2));
+    //rects.push_back(pack_4(0, 0, 1, 1));
+    //rects.push_back(pack_4(0, 1, 1, 2));
+    //rects.push_back(pack_4(1, 0, 2, 1));
+    //rects.push_back(pack_4(1, 1, 2, 2));
+/*
+    rects.push_back(pack_4(0, 0, 2, 2));
+    rects.push_back(pack_4(0, 2, 1, 3));
+    rects.push_back(pack_4(1, 2, 2, 3));
+    rects.push_back(pack_4(2, 2, 3, 3));
+    rects.push_back(pack_4(2, 0, 3, 1));
+    rects.push_back(pack_4(2, 1, 3, 2));
+*/
+    // Print rects data
+    for (auto & rect : rects) {
+        print_rect(rect);
     }
 
-    // some magic
-    uint32_t min_count = p;
-    for (uint32_t i = 0; i < xrects.size(); ++i) {
-        if (xrects[i][0] == 0) {
-            // call recurrent function
-            uint32_t ind = i;
-            result_type tree_results = walkthrough(n, m, ind, xrects);
-            for (auto & result : tree_results) {
-                uint32_t square = result & 0x0000FFFF;
-                uint32_t counter = result >> 16;
-                if (square == n * m) {
-                    min_count = min(min_count, counter);
-                }
+    set <uint32_t> prev;
+
+    // Iterate through rects
+    for (auto & rect : rects) {
+        // If rect started at zero
+        if ((rect >> 16) == 0) {
+            // Unpack info about current rectangle
+            // Coords
+            array <uint8_t, 4> coords = unpack_4(rect);
+            uint8_t x1 = coords[0];
+            uint8_t y1 = coords[1];
+            uint8_t x2 = coords[2];
+            uint8_t y2 = coords[3];
+            // Vertices
+            uint16_t ll = (x1 << 8) | y1;
+            uint16_t lr = (x2 << 8) | y1;
+            uint16_t ul = (x1 << 8) | y2;
+            uint16_t ur = (x2 << 8) | y2;
+            // Square
+            uint16_t square = (x2 - x1) * (y2 - y1);
+            // Zero-point solution = prev
+            uint16_t counter = 1;
+            uint32_t score = square | (counter << 16);
+            prev.insert(score);
+            // Call recurrent function
+            prev = walkthrough(data, prev, rect, rects);
+            // Remove incorrect solutions
+            check_solution(data, prev);
+            // Iterate through results
+            uint16_t min_count = p;
+            for (auto & result : prev) {
+                array <uint16_t, 2> scores = unpack_2(result);
+                uint16_t square = scores[1];
+                uint16_t counter = scores[0];
+                if (square == n*m) min_count = min(min_count, counter);
             }
+            min_count = min_count == p ? -1 : min_count;
+            cout << min_count << endl;
         }
     }
-    cout << min_count << endl;
 }
