b50b61da9377c04da2120a3c247c9a87e02708e9
Alexander Abrosov <aabrosov@gmail.com>
2022-09-28 21:20:44 +0300

    header added

---
+++ src/test0.hpp
@@ -0,0 +1,7 @@
+/* File test0.hpp */
+#ifndef TEST0_HPP
+#define TEST0_HPP
+ 
+void unpack_solution(const uint32_t & solution, int & square, int & counter, int & x, int & y);
+ 
+#endif /* TEST0_HPP */
