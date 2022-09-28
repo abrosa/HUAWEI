478bd8023aa0ad425cbde794e54438c737ea9e19
Alexander Abrosov <aabrosov@gmail.com>
2022-09-28 15:11:59 +0300

    some changes, draft, not tested

--- src/test3_optimized.cpp
+++ src/test3_optimized.cpp
@@ -11,11 +11,6 @@ class Solutions {
         int m;
         // number of fragments
         int p;
-        // coordinates
-        uint8_t x1;
-        uint8_t y1;
-        uint8_t x2;
-        uint8_t y2;
         // rect with packed coordinates
         uint32_t rect;
         // info about corners
@@ -23,168 +18,117 @@ class Solutions {
         uint16_t c_lr;
         uint16_t c_ul;
         uint16_t c_ur;
-        // rect square and counter
-        uint16_t square;
-        uint16_t counter;
+        // rect score = square
         vector <int> score;
-        // flag for processed rects
-        bool processed = false;
-        bool is_ll = false;
-        bool is_lr = false;
-        bool is_ul = false;
-        bool is_ur = false;
-        // info about neighbours
-        vector <Solutions> neighbours;
+        // info about children
+        vector <Solutions> children;
+        //
+        bool processed;
 };
-
-void merge_solutions(Solutions & curr, Solutions & next) {
-    // merge curr and next score
-    vector <int> score1, score2;
-    score1 = curr.score;
-    score2 = next.score;
-    while (score1.size > score2.size) score2.push_back(score2.size() + 1);
-    while (score2.size > score1.size) score1.push_back(score1.size() + 1);
-    // multiply solution matrices
-    for (auto & score1 : curr.score) {
-        uint16_t square1 = score1 & 65535;
-        uint16_t counter1 = score1 >> 16;
-        for (auto & score2 : next.score) {
-            uint16_t square2 = score2 & 65535;
-            uint16_t counter2 = score2 >> 16;
-            if (square1 + square2 <= curr.n * curr.m && counter1 + counter2 <= curr.p) {
-                //cout << "!";
-                tmp.insert(score1 + score2);
-            }
-        }
-    }
-    curr.score = score;
-    next.score = score;
-}
 
 vector <Solutions> collect_info(int n, int m, vector <uint32_t> & rects) {
     // collect info for Solutions class
     vector <Solutions> vsols;
     for (auto & x : rects) {
         Solutions curr;
-        curr.n = n;
-        curr.m = m;
-        curr.p = rects.size();
-        curr.rect = x;
-        curr.x1 = x >> 24;
-        curr.y1 = x >> 16 & 255;
-        curr.x2 = x >> 8 & 255;
-        curr.y2 = x & 255;
-        curr.c_ll = (curr.x1 << 8) + curr.y1;
-        curr.c_lr = (curr.x2 << 8) + curr.y1;
-        curr.c_ul = (curr.x1 << 8) + curr.y2;
-        curr.c_ur = (curr.x2 << 8) + curr.y2;
-        curr.is_ll = curr.x1 == 0 && curr.y1 == 0;
-        curr.is_lr = curr.x2 == n && curr.y1 == 0;
-        curr.is_ul = curr.x1 == 0 && curr.y2 == m;
-        curr.is_ur = curr.x2 == n && curr.y2 == m;
-        curr.square = (curr.x2 - curr.x1) * (curr.y2 - curr.y1);
-        curr.counter = 1;
-        uint32_t score = curr.square + (1<<16);
-        curr.score.insert({score});
+            // initial data
+            curr.n = n;
+            curr.m = m;
+            curr.p = rects.size();
+            // packed rect
+            curr.rect = x;
+            // unpack rect
+            uint8_t x1 = x >> 24;
+            uint8_t y1 = x >> 16 & 255;
+            uint8_t x2 = x >> 8 & 255;
+            uint8_t y2 = x & 255;
+            // corners
+            curr.c_ll = (x1 << 8) + y1;
+            curr.c_lr = (x2 << 8) + y1;
+            curr.c_ul = (x1 << 8) + y2;
+            curr.c_ur = (x2 << 8) + y2;
+            // score = rect square
+            curr.score.push_back((x2 - x1) * (y2 - y1));
+            // processed flag
+            curr.processed = false;
         vsols.push_back(curr);
     }
     return vsols;
 }
 
-void update_info(vector <Solutions> & vsols) {
-    // update info about neighbours
-    for (auto & curr : vsols) {
-        for (auto & next : vsols) {
-            if ((curr.c_ul == next.c_ur) ||
-                (curr.c_lr == next.c_ur) ||
-                (curr.c_ur == next.c_ur) ||
-                (curr.c_ur == next.c_ul) ||
-                (curr.c_ll == next.c_ul) ||
-                (curr.c_ul == next.c_ul) ||
-                (curr.c_ur == next.c_lr) ||
-                (curr.c_ll == next.c_lr) ||
-                (curr.c_lr == next.c_lr) ||
-                (curr.c_ul == next.c_ll) ||
-                (curr.c_lr == next.c_ll) ||
-                (curr.c_ll == next.c_ll))
-                curr.neighbours.push_back(next);
+void merge_solutions(Solutions & curr, Solutions & next) {
+    //vector <int> temp_score;
+    int new_size = curr.score.size() + next.score.size();
+    new_size = min(new_size, curr.p);
+    next.score.resize(new_size);
+    //temp_score.clear();
+    //int tmp
+    for (int i = 0; i < next.score.size(); ++i) {
+    for (int j = i; j < i + curr.score.size(); ++j) {
+        if (next.score[i] + curr.score[j-i] <= curr.n * curr.m && j + 2 <= curr.p) {
+            next.score[j+1] = max(next.score[j+1], next.score[i] + curr.score[j-i]);
         }
     }
+    }
+    next.score[0] = max(next.score[0], curr.score[0]);
+    //next.score = temp_score;
 }
 
-void visualize(vector <Solutions> & vsols) {
-    string tmp;
-    //system("ping 127.0.0.1 -n 1 > nul");
-    uint32_t rect;
-    for (int y = vsols[0].m - 1; y >= 0; --y) {
-        for (int x = 0; x < vsols[0].n; ++x) {
-            rect = x * 256 * 65537 + y * 65537 + 257;
-            for (auto & curr : vsols) {
-                if (curr.rect == rect) {
-                    if (curr.processed) {
-                        tmp += "X";
-                    } else {
-                        tmp += "-";
-                    }
-                    tmp += " ";
-                }
-            }
-        }
-        tmp += "\n";
+void recurse(Solutions & current, vector <Solutions> & vsols) {
+    if (current.processed) return;
+cout << endl << "vsols size and curr children size (" << vsols.size() << "'" << current.children.size() << ")" << endl;
+cout << "rect [" << int(current.rect>>24) << "'" << int(current.rect>>16&255) << "'" << int(current.rect>>8&255) << "'" << int(current.rect&255) << "]" << endl;
+cout << "curr square = score      <"; for (int i = 0; i < current.score.size(); ++i) cout << "<" << current.score[i] << ">"; cout << ">" << endl;
+    for (auto next = current.children.begin(); next != current.children.end(); ++next) {
+cout << "    next before merge    {"; for (int i = 0; i < (*next).score.size(); ++i) cout << "<" << (*next).score[i] << ">"; cout << "}" << endl;
+        merge_solutions(current, *next);
+cout << "        next after merge ("; for (int i = 0; i < (*next).score.size(); ++i) cout << "<" << (*next).score[i] << ">"; cout << ")" << endl;
+        recurse(*next, vsols);
     }
-    tmp += "\n";
-    system("cls");
-    cout << tmp;
+    current.processed = true;
 }
 
 int walkthrough(int & n, int & m, vector <uint32_t> & rects) {
-    // collect info from the rects
     vector <Solutions> vsols = collect_info(n, m, rects);
-    update_info(vsols);
-    // main loop
-    int counter = 10;
-    int processed;
+    for (auto & curr : vsols) {
+        curr.children.clear();
+        for (auto & next : vsols) {
+            if ((curr.c_ul == next.c_ll) || (curr.c_lr == next.c_ll))
+                curr.children.push_back(next);
+        }
+    }
+    int counter = 5;
     do {
-        processed = 0;
+        //for (auto & curr : vsols) {
+        //    curr.children.clear();
+        //    for (auto & next : vsols) {
+        //        if ((curr.c_ul == next.c_ll) || (curr.c_lr == next.c_ll))
+        //            curr.children.push_back(next);
+        //    }
+        //}
         for (auto curr = vsols.begin(); curr != vsols.end(); ++curr) {
-            for (auto next = (*curr).neighbours.begin(); next != (*curr).neighbours.end(); ++next) {
-                merge_solutions(*curr, *next); ++processed;
+            if ((*curr).processed) continue;
+            for (auto next = (*curr).children.begin(); next != (*curr).children.end(); ++next) {
+                merge_solutions(*curr, *next);
+                recurse(*next, vsols);
             }
-
-            //visualize(vsols);
-            cout << vsols.size() << endl;
-            for (auto & vsol : vsols) {
-                for (auto & score : vsols[0].score) {
-                    auto square = score & 65535;
-                    auto cou    = score >> 16;
-                    cout << square << "'" << cou << " ";
-                }
-                cout << ";";
-            }
-            cout << endl;
-
-            if (vsols.size() > 1) {
-                vsols.erase(curr);
-                --curr;
-            }
+            (*curr).processed = true;
+            //if (vsols.size() > 1) {
+            //    vsols.erase(curr);
+            //    --curr;
+            //}
         }
     } while (counter --> 0);
-    //} while (processed != 0);
-    //cout << vsols.size() << endl;
-    // Iterate through solutions to find the best
-    int p = rects.size();
-    int min_count = p + 1;
+    int min_count = rects.size() + 1;
     for (auto & solution : vsols) {
-        if (solution.processed) continue;
         for (auto & curr_solution : solution.score) {
-            int square = curr_solution & 65535;
-            int counter = curr_solution >> 16;
-            if (square == n * m) {
+            if (curr_solution == n * m) {
                 min_count = min(min_count, counter);
             }
         }
     }
-    return min_count == p + 1 ? -1 : min_count;
+    cout << endl;
+    return min_count == rects.size() + 1 ? -1 : min_count;
 }
 
 void pack_rect(int x1, int y1, int x2, int y2, vector <uint32_t> & rects) {
@@ -193,82 +137,20 @@ void pack_rect(int x1, int y1, int x2, int y2, vector <uint32_t> & rects) {
 }
 
 int main() {
-    // Map size
     int n = 5, m = 5;
-    // Fill rects
     vector <uint32_t> rects;
-/*
     for (int i = 0; i < n; i += 1){
         for (int j = 0; j < m; j += 1){
             pack_rect(i, j, i + 1, j + 1, rects);
         }
     }
-*/
-/*
-    for (int i = 0; i < n; i += 3){
-        for (int j = 0; j < m; j += 3){
-            pack_rect(i, j, i + 3, j + 3, rects);
-        }
-    }
-*/
-pack_rect(0,0,1,1,rects);
-pack_rect(0,1,1,2,rects);
-pack_rect(0,2,1,3,rects);
-pack_rect(0,3,1,4,rects);
-pack_rect(0,4,1,5,rects);
-pack_rect(1,0,2,1,rects);
-pack_rect(1,1,2,2,rects);
-pack_rect(1,2,2,3,rects);
-pack_rect(1,3,2,4,rects);
-pack_rect(1,4,2,5,rects);
-pack_rect(2,0,3,1,rects);
-pack_rect(2,1,3,2,rects);
-pack_rect(2,2,3,3,rects);
-pack_rect(2,3,3,4,rects);
-pack_rect(2,4,3,5,rects);
-pack_rect(3,0,4,1,rects);
-pack_rect(3,1,4,2,rects);
-pack_rect(3,2,4,3,rects);
-pack_rect(3,3,4,4,rects);
-pack_rect(3,4,4,5,rects);
-pack_rect(4,0,5,1,rects);
-pack_rect(4,1,5,2,rects);
-pack_rect(4,2,5,3,rects);
-pack_rect(4,3,5,4,rects);
-pack_rect(4,4,5,5,rects);
 
-pack_rect(0,0,2,1,rects);
-pack_rect(0,1,2,2,rects);
-pack_rect(0,2,2,3,rects);
-pack_rect(0,3,2,4,rects);
-pack_rect(0,4,2,5,rects);
-pack_rect(1,0,3,1,rects);
-pack_rect(1,1,3,2,rects);
-pack_rect(1,2,3,3,rects);
-pack_rect(1,3,3,4,rects);
-pack_rect(1,4,3,5,rects);
-pack_rect(2,0,4,1,rects);
-pack_rect(2,1,4,2,rects);
-pack_rect(2,2,4,3,rects);
-pack_rect(2,3,4,4,rects);
-pack_rect(2,4,4,5,rects);
-pack_rect(3,0,5,1,rects);
-pack_rect(3,1,5,2,rects);
-pack_rect(3,2,5,3,rects);
-pack_rect(3,3,5,4,rects);
-pack_rect(3,4,5,5,rects);
-    // Print result
+/*
+pack_rect(0,0,3,2,rects);
+pack_rect(3,0,5,3,rects);
+pack_rect(0,2,3,5,rects);
+pack_rect(3,3,5,5,rects);
+*/
+
     cout << walkthrough(n, m, rects) << endl;
-    system("pause");
-    return 0;
 }
-
-
-/*
- ||
-                    ((*next1).lr == (*curr).ll || (*next1).lr == (*curr).ur) ||
-                    ((*next1).ul == (*curr).ll || (*next1).ul == (*curr).ur) ||
-                    ((*next1).ur == (*curr).lr || (*next1).ur == (*curr).ul)
-
-
-*/
